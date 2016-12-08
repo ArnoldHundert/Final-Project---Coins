4adc353135013fa8b78d1c70fbbce067a2c67721
Arnold Hundert <hunderta@yahoo.com>
2016-12-08 18:20:53 -0500

    Getting further along. Working on adding paperclip to app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-
+  def configure_devise_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname, :address1, :address2, :city, :state, :zipcode, :phnumber, :usertype])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,28 @@ class PostsController < ApplicationController
     # @message = "Staten Island Coin Club"
   end
 
+  def create
+    @post = Post.create( post_params )
+  end
+
+
+  * new
+  * show
+  * edit
+  * update
+  * destroy
+
+
+
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update()
+  # controller methods.
+
+  def post_params
+    params.require(:post).permit(:title, :image, :post, :user_id)
+  end
+
 end
